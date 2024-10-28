bfcbf93bfda674d869416a060454602dce06aef6
Elmir Dzaka <43626153+ElmirDzaka@users.noreply.github.com>
2024-10-28 15:03:20 -0600

    Update work to match GPIO_Interrupt

--- src/timer.c
+++ src/timer.c
@@ -7,7 +7,7 @@ bool timer_callback(__unused struct repeating_timer *t)
 {
     toggle = !toggle;
     gpio_put(OUT_PIN, toggle);
-    for (int i = 0; i < 500000; i++);
+    for (int i = 0; i < 1000000; i++);
     return true;
 }
 
